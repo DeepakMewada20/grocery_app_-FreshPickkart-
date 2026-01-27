2597f68facd4f2eb8e4159b3aa89d61d04e8ac23
deepakmewada20 <mewadadeepak367@gmail.com>
2026-01-27 17:31:01 +0530

    I add slpash screen and bottom sheet for nevigation

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Categories'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'Categories Screen',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
