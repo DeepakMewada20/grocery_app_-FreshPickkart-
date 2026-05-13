5cbe707512f30864b86466f367d4e7dfe679d573
deepakmewada20 <mewadadeepak367@gmail.com>
2026-05-13 15:27:16 +0530

    I add api in my url like flutter run --dart-define=API_BASE_URL=http:///api/

--- freshpickkat_flutter/run_user_prod.sh
+++ freshpickkat_flutter/run_user_prod.sh
@@ -4,8 +4,8 @@
 REMOTE_IP="144.217.241.119"
 
 echo "🌐 Using Remote Production IP: $REMOTE_IP"
-echo "🚀 Running User App with --dart-define=API_BASE_URL=http://$REMOTE_IP:8080/"
+echo "🚀 Running User App with --dart-define=API_BASE_URL=http://$REMOTE_IP/api/"
 echo ""
 
 # Run the app
-flutter run --dart-define=API_BASE_URL=http://$REMOTE_IP:8080/
+flutter run --dart-define=API_BASE_URL=http://$REMOTE_IP/api/
