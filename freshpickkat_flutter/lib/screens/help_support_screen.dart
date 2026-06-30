abbdaa8083d5e48f9d601689902813234a5530de
deepakmewada20 <mewadadeepak367@gmail.com>
2026-06-30 16:34:27 +0530

    chore: update customer support phone number and WhatsApp contact link

--- freshpickkat_flutter/lib/screens/help_support_screen.dart
+++ freshpickkat_flutter/lib/screens/help_support_screen.dart
@@ -21,7 +21,7 @@ class HelpSupportScreen extends StatefulWidget {
 }
 
 class _HelpSupportScreenState extends State<HelpSupportScreen> {
-  static const _supportPhone = '+919109042266';
+  static const _supportPhone = '+919893741160';
   static const _supportEmail = 'freshpickkart@gmail.com';
 
   late final Future<PackageInfo> _appInfoFuture;
@@ -285,7 +285,7 @@ class _HelpSupportScreenState extends State<HelpSupportScreen> {
       'Hi FreshPickKat Support, I need help.',
     );
     await _launchExternal(
-      Uri.parse('https://wa.me/919109042266?text=$text'),
+      Uri.parse('https://wa.me/919893741160?text=$text'),
       fallbackMessage: 'WhatsApp is not available on this device.',
     );
   }
